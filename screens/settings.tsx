fb056d8d65cdd6961a40b5f51f618b8a1d01cf70
Sean Walsh <sean@dronedeploy.com>
2018-05-28 15:26:12 -0700

    allow clearing all entries

--- screens/settings.tsx
+++ screens/settings.tsx
@@ -1,24 +1,48 @@
 import React from 'react';
 import {Component} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Alert} from 'react-native';
 import {NavigationScreenProps} from 'react-navigation';
+import {Button} from 'react-native-elements';
 
 import {Color} from '../colors';
+import {TrackedItem, realm} from '../model/realm';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: Color.White,
+  },
+  buttonContainer: {
+    width: '100%',
+    marginBottom: 8,
   },
 });
 
 export default class SettingsScreen extends Component<NavigationScreenProps> {
+  onDeleteClicked = () => {
+    Alert.alert('Are you sure?', 'Once you delete your tracked items, they cannot be recovered.', [
+      {text: 'Cancel'},
+      {text: 'Delete', onPress: this.onDeleteConfirmed}
+    ])
+  }
+
+  onDeleteConfirmed = () => {
+    const items = TrackedItem.all();
+
+    realm.write(() => realm.delete(items));
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>testing</Text>
+        <Button
+          containerViewStyle={styles.buttonContainer}
+          buttonStyle={{backgroundColor: Color.Red, padding: 12, justifyContent: 'space-between'}}
+          iconRight={{name: 'trash', type: 'font-awesome'}}
+          title="Delete all entries"
+          onPress={this.onDeleteClicked}
+        />
       </View>
     );
   }
