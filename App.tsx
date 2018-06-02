d530d45d98beb2856270674f9c56d86be3a99de9
Sean Walsh <sean@dronedeploy.com>
2018-06-02 16:46:35 -0700

    wire react-navigation into redux

--- App.tsx
+++ App.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { Component } from 'react'
 import { StyleSheet, View } from 'react-native'
 import { Header } from 'react-native-elements'
+import { initializeListeners } from 'react-navigation-redux-helpers'
+import { Provider, connect, Dispatch } from 'react-redux'
+import { DrawerActions, NavigationActions, NavigationDispatch, NavigationState } from 'react-navigation'
 
 import { Color } from './colors'
 import { RootDrawer } from './routes'
-import Navigation from './navigation'
 
 import HeaderIcon from './components/header-icon'
+import { AppState, navigationPropConstructor, ROOT, store } from './store'
 
 const styles = StyleSheet.create({
   container: {
@@ -16,23 +19,53 @@ const styles = StyleSheet.create({
   },
 })
 
-export default class App extends Component {
+interface Props {
+  nav: NavigationState
+  dispatch: Dispatch & NavigationDispatch
+}
+
+class App extends Component<Props> {
+  componentDidMount() {
+    initializeListeners(ROOT, this.props.nav)
+  }
+
   render() {
+    const navigation = navigationPropConstructor(this.props.dispatch, this.props.nav)
+    const routeIndex = this.props.nav.routes[this.props.nav.index].index
+    const isBackable = !!routeIndex && routeIndex !== 0
+    const leftIcon = isBackable ? 'arrow-back' : 'menu'
+    const leftAction = isBackable ? NavigationActions.back() : DrawerActions.toggleDrawer()
+
     return (
       <View style={styles.container}>
         <Header
           leftComponent={
-            <HeaderIcon name="menu" onClick={() => Navigation.toggleDrawer()} />
+            <HeaderIcon
+              name={leftIcon}
+              onClick={() => this.props.dispatch(leftAction)}
+            />
           }
           centerComponent={{
             text: 'LIFE',
             style: { color: Color.White },
           }}
-          rightComponent={<HeaderIcon name="help-outline" onClick={() => {}} />}
           backgroundColor={Color.Black}
         />
-        <RootDrawer ref={(ref: any) => Navigation.setNav(ref)} />
+        <RootDrawer navigation={navigation} />
       </View>
+    )
+  }
+}
+
+const mapStateToProps = (state: AppState) => ({ nav: state.nav })
+const AppWithNavigationState = connect(mapStateToProps)(App)
+
+export default class Root extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <AppWithNavigationState />
+      </Provider>
     )
   }
 }
