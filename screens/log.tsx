3f5dce167b5393b4621e41fad5d13825739e0495
Sean Walsh <sean@dronedeploy.com>
2018-06-02 17:14:26 -0700

    allow deleting tracked items

--- screens/log.tsx
+++ screens/log.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Component } from 'react'
-import { StyleSheet, View, SectionList, ListRenderItemInfo, SectionListData } from 'react-native'
+import { StyleSheet, View, SectionList, ListRenderItemInfo, SectionListData, Alert, ToastAndroid } from 'react-native'
 import { NavigationScreenProps } from 'react-navigation'
 import { ListItem } from 'react-native-elements'
 import { format } from 'date-fns'
 
 import { Color } from '../colors'
-import { TrackedItem, TrackedItemModel } from '../model/realm'
+import { realm, TrackedItem, TrackedItemModel } from '../model/realm'
 import { LogHeader } from '../components/log/log-header'
 
 const styles = StyleSheet.create({
@@ -28,9 +28,6 @@ const keyExtractor = (item: TrackedItemModel) => item.id
 const renderHeader = ({ section: { title } }: { section: SectionListData<TrackedItemModel> }) => (
   <LogHeader title={title} />
 )
-const renderItem = ({ item }: ListRenderItemInfo<TrackedItemModel>) => (
-  <ListItem title={item.item.name} subtitle={format(item.timestamp, 'h:mm:ss a')} hideChevron />
-)
 const groupItemsByDay = (items: ReadonlyArray<TrackedItemModel>) =>
   items.reduce((accum: TrackedItemsByDate, item) => {
     const date = format(item.timestamp, 'MM/DD/YY')
@@ -46,9 +43,47 @@ const groupItemsByDay = (items: ReadonlyArray<TrackedItemModel>) =>
 const createSections = (groupedItems: TrackedItemsByDate) =>
   Object.keys(groupedItems).map((key) => ({ title: key, data: groupedItems[key] }))
 
-export default class LogScreen extends Component<NavigationScreenProps> {
+interface State {
+  items: ReadonlyArray<TrackedItemModel>
+}
+
+export default class LogScreen extends Component<NavigationScreenProps, State> {
+  state = {
+    items: [],
+  }
+
+  componentWillMount() {
+    this.loadItems()
+  }
+
+  loadItems() {
+    this.setState((state) => ({ ...state, items: TrackedItem.all() }))
+  }
+
+  renderItem = ({ item }: ListRenderItemInfo<TrackedItemModel>) => (
+    <ListItem
+      title={item.item.name}
+      subtitle={format(item.timestamp, 'h:mm:ss a')}
+      hideChevron
+      onPress={() => ToastAndroid.show('Long press an item to delete it.', ToastAndroid.SHORT)}
+      onLongPress={() => this.onItemLongPressed(item)}
+    />
+  )
+
+  onItemLongPressed = (item: TrackedItemModel) => {
+    Alert.alert('Are you sure?', 'Once you delete this item, it cannot be recovered.', [
+      { text: 'Cancel' },
+      { text: 'Delete', onPress: () => this.onDeleteConfirmed(item) },
+    ])
+  }
+
+  onDeleteConfirmed = (item: TrackedItemModel) => {
+    realm.write(() => realm.delete(item))
+    this.loadItems()
+  }
+
   render() {
-    const sections = createSections(groupItemsByDay(TrackedItem.all()))
+    const sections = createSections(groupItemsByDay(this.state.items))
 
     return (
       <View style={styles.container}>
@@ -57,7 +92,7 @@ export default class LogScreen extends Component<NavigationScreenProps> {
           keyExtractor={keyExtractor}
           sections={sections}
           renderSectionHeader={renderHeader}
-          renderItem={renderItem}
+          renderItem={this.renderItem}
         />
       </View>
     )
