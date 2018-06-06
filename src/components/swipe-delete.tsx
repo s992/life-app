7f2a0301d924252307259b3288cefbc34a446726
Sean Walsh <sean@dronedeploy.com>
2018-06-05 21:58:53 -0700

    swipe to delete wip

---
+++ src/components/swipe-delete.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { StyleSheet } from 'react-native'
+import { Icon } from 'react-native-elements'
+import Swipeout from 'react-native-swipeout'
+
+import { Color } from '../colors'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
+
+interface Props {
+  children: any
+  onClick: () => void
+}
+
+export const SwipeDelete = (props: Props) => (
+  <Swipeout
+    backgroundColor={Color.White}
+    right={[
+      {
+        component: <Icon name="delete" color={Color.White} containerStyle={styles.container} />,
+        backgroundColor: Color.Red,
+        onPress: props.onClick,
+      },
+    ]}
+  >
+    {props.children}
+  </Swipeout>
+)
--- src/screens/manage-events.tsx
+++ src/screens/manage-events.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Component } from 'react'
-import { StyleSheet, ListRenderItemInfo, Alert, ScrollView } from 'react-native'
+import { StyleSheet, Alert, ScrollView } from 'react-native'
 import { NavigationScreenProps } from 'react-navigation'
-import { ListItem } from 'react-native-elements'
+import { Icon, ListItem } from 'react-native-elements'
 import { connect, DispatchProp } from 'react-redux'
+import Swipeout from 'react-native-swipeout'
 
 import { Color } from '../colors'
 import { Event, EventModel } from '../model/realm'
@@ -27,13 +28,17 @@ class ManageEventsScreen extends Component<NavigationScreenProps & DispatchProp,
     events: Event.all(),
   }
 
-  renderItem = ({ item }: ListRenderItemInfo<EventModel>) => (
-    <ListItem
-      title={item.name}
-      onPress={() => this.onEventPressed(item)}
-      onLongPress={() => this.onEventLongPressed(item)}
-    />
-  )
+  getDeleteButton = (event: EventModel) => ({
+    component: (
+      <Icon
+        name="delete"
+        color={Color.White}
+        containerStyle={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+      />
+    ),
+    backgroundColor: Color.Red,
+    onPress: () => this.onDeleteRequested(event),
+  })
 
   onEventPressed = (event: EventModel) => {
     this.props.navigation.navigate(Screen.EventDetail, {
@@ -41,7 +46,7 @@ class ManageEventsScreen extends Component<NavigationScreenProps & DispatchProp,
     })
   }
 
-  onEventLongPressed = (event: EventModel) => {
+  onDeleteRequested = (event: EventModel) => {
     Alert.alert(
       'Are you sure?',
       'Once you delete this event, all log entries associated with it will be deleted as well.',
@@ -58,12 +63,13 @@ class ManageEventsScreen extends Component<NavigationScreenProps & DispatchProp,
     return (
       <ScrollView style={styles.container}>
         {this.state.events.map((event) => (
-          <ListItem
-            key={event.id}
-            title={event.name}
-            onPress={() => this.onEventPressed(event)}
-            onLongPress={() => this.onEventLongPressed(event)}
-          />
+          <Swipeout key={event.id} backgroundColor={Color.White} right={[this.getDeleteButton(event)]}>
+            <ListItem
+              title={event.name}
+              onPress={() => this.onEventPressed(event)}
+              onLongPress={() => this.onDeleteRequested(event)}
+            />
+          </Swipeout>
         ))}
       </ScrollView>
     )
