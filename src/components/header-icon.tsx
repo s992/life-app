4a529a667a2395792d229f78c6ae658d91b8f09f
Sean Walsh <sean@dronedeploy.com>
2018-06-13 21:21:51 -0700

    add hitslop to header button

--- src/components/header-icon.tsx
+++ src/components/header-icon.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { TouchableWithoutFeedback } from 'react-native'
 import { Icon } from 'react-native-elements'
 
 import { Color } from '../colors'
@@ -8,6 +9,15 @@ export interface Props {
   onClick: () => void
 }
 
+const hitSlop = {
+  top: 20,
+  left: 20,
+  bottom: 20,
+  right: 20,
+}
+
 export const HeaderIcon = (props: Props) => (
-  <Icon name={props.name} color={Color.White} underlayColor={Color.Black} onPress={() => props.onClick()} />
+  <TouchableWithoutFeedback onPress={() => props.onClick()} hitSlop={hitSlop}>
+    <Icon name={props.name} color={Color.White} underlayColor={Color.Black} />
+  </TouchableWithoutFeedback>
 )
