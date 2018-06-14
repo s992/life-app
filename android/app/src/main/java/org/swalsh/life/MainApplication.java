8fa434874eb45d58af3deaffb2ddfd924434a619
Sean Walsh <sean@dronedeploy.com>
2018-06-13 18:28:46 -0700

    install snackbar

--- android/app/src/main/java/org/swalsh/life/MainApplication.java
+++ android/app/src/main/java/org/swalsh/life/MainApplication.java
@@ -3,6 +3,7 @@ package org.swalsh.life;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.azendoo.reactnativesnackbar.SnackbarPackage;
 import com.calendarevents.CalendarEventsPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SnackbarPackage(),
             new CalendarEventsPackage(),
             new SplashScreenReactPackage(),
             new RNFetchBlobPackage(),
